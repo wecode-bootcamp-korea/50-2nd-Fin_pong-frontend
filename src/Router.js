d8b9fd66af03b911b81d84bb3ca21054a46cbcfc
이승윤 <iseung-yun@iseung-yun-ui-MacBookPro.local>
2023-10-16 12:54:06 +0900

    initial settings

---
+++ src/Router.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Nav from './components/Nav/Nav';
+import Main from './pages/Main/Main';
+
+const Router = () => {
+  return (
+    <BrowserRouter>
+      <Nav />
+      <Routes>
+        <Route path="/" element={<Main />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
