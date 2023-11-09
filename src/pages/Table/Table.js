1ef6c7e3d9e0cd255c6b7ce9f314e8103d3afa78
Saemi Lee <saemi@Saemiui-MacBookPro.local>
2023-11-09 12:01:52 +0900

    Add: 페이지별 초기 세팅 완료

---
+++ src/pages/Table/Table.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import './Table.scss';
+
+const Table = () => {
+  return <div>Table</div>;
+};
+
+export default Table;
+
---
+++ src/pages/UserDetail/UserDetail.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './UserDetail.scss';
+
+const UserDetail = () => {
+  return <div>UserDetail</div>;
+};
+
+export default UserDetail;
+
+
