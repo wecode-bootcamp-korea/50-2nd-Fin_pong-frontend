82c51d9b82dd142f956b24b795ca371442d20fe2
Saemi <saemi@Saemiui-MacBookPro.local>
2023-11-12 20:35:14 +0900

    Add: 추가정보 기입 페이지 기본 구현 완료

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,14 @@
 import React from 'react';
+// import KakaoLogin from 'react-kakao-login';
+import KakaoLogin from './KakaoLogin/KakaoLogin';
+
 import './Login.scss';
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <KakaoLogin />
+    </div>
+  );
 };
-
 export default Login;
--- src/pages/UserDetail/UserDetail.js
+++ src/pages/UserDetail/UserDetail.js
@@ -1,8 +1,66 @@
 import React from 'react';
+import { useState } from 'react';
 import './UserDetail.scss';
+// import DropDown from './Dropdown';
 
 const UserDetail = () => {
-  return <div>UserDetail</div>;
+  const [userInfo, setUserInfo] = useState({
+    userNameme: '',
+    userPhoneNumber: '',
+    userBirthDate: '', // 문자열인지 체크
+    userAuth: '',
+  });
+
+  const handleSubmit = () => {
+    fetch(api, {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(userInfo),
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        if (response.message === 'SIGN_IN_COMPLETED') {
+          alert('환영합니다!');
+        } else {
+          alert('빠진 정보가 있는지 확인해보세요!');
+        }
+      });
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setUserInfo({ ...userInfo, [name]: value });
+  };
+
+  const FAMILY_MEMBER = [
+    { id: 1, family: '부모' },
+    { id: 2, family: '자녀' },
+  ];
+
+  return (
+    <div className="UserDetail">
+      <div className="userDetailWrapperText">
+        <h1>추가 정보 등록하기</h1>
+      </div>
+      <div className="userNameWrapper">
+        <input className="userName" placeholder="이름" type="text" />
+        <input className="userPhoneNumber" placeholder="전화번호" type="text" />
+        <input className="userBirthDate" placeholder="생년월일" type="text" />
+        <select className="userAuth">
+          {FAMILY_MEMBER.map((family) => (
+            <option key={family.id} value={family.family}>
+              {family.family}
+            </option>
+          ))}
+        </select>
+      </div>
+      <div className="buttonSignUp">
+        <button>회원 등록 완료</button>
+      </div>
+    </div>
+  );
 };
 
 export default UserDetail;
