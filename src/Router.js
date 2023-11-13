179668618cb828b99bc1d90b586c6e07a0ebe5c2
김성호 <rlatjdgh9612@naver.com>
2023-11-13 16:12:43 +0900

    Add: MenuBar 페이지 제작

--- src/Router.js
+++ src/Router.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Nav from './components/Nav/Nav';
+// import Nav from './components/Nav/Nav';
 import Main from './pages/Main/Main';
+import MenuBar from './pages/MenuBar/MenuBar';
 
 const Router = () => {
   return (
     <BrowserRouter>
-      <Nav />
+      {/* <Nav /> */}
       <Routes>
         <Route path="/" element={<Main />} />
+        <Route path="/menubar" element={<MenuBar />} />
       </Routes>
     </BrowserRouter>
   );
