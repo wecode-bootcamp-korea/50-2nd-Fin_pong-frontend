7e3d31340dc439d3d15f0e1b4a380ca8b5ccd29d
김성호 <rlatjdgh9612@naver.com>
2023-11-21 18:31:34 +0900

    Fix: GraphCircularChart 로직 수정(차트 중간에 월 표시, 데이터별 Text 표시)

--- src/pages/Main/GraphCircularChart.js
+++ src/pages/Main/GraphCircularChart.js
@@ -1,8 +1,27 @@
-import React, { PureComponent } from 'react';
-import { PieChart, Pie, Cell, Tooltip, LabelList, Label, Bar } from 'recharts';
+import React, { useState, useEffect } from 'react';
+import { PieChart, Pie, Cell, Tooltip, LabelList } from 'recharts';
 
 const GraphCircularChart = ({ data }) => {
   const transformedData = [];
+  // 월 데이터 변환 로직
+  const [currentMonth, setCurrentMonth] = useState(getCurrentMonth());
+  // 현재
+  useEffect(() => {
+    const interval = setInterval(() => {
+      const newMonth = getCurrentMonth();
+      if (newMonth !== currentMonth) {
+        setCurrentMonth(newMonth);
+      }
+    }, 1000 * 60);
+
+    return () => clearInterval(interval);
+  }, [currentMonth]);
+
+  function getCurrentMonth() {
+    const currentDate = new Date();
+    const month = currentDate.toLocaleString('default', { month: 'long' });
+    return month;
+  }
 
   data.forEach((item) => {
     let value = 0;
@@ -18,13 +37,15 @@ const GraphCircularChart = ({ data }) => {
   });
 
   const total = data.reduce((acc, entry) => acc + entry.value, 0);
+  console.log('total', total);
+
   return (
     <PieChart width={900} height={500}>
       <Pie
         data={transformedData}
         dataKey="value"
         isAnimationActive={true}
-        cx={300}
+        cx={250}
         cy={140}
         innerRadius={45}
         outerRadius={110}
@@ -35,13 +56,46 @@ const GraphCircularChart = ({ data }) => {
         {transformedData.map((entry, index) => (
           <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
         ))}
-        <LabelList dataKey="name" position="inside" />
+        <LabelList
+          dataKey="name"
+          position="outside"
+          content={(props) => {
+            const { value, index } = props;
+            const percent = data[index].spending;
+
+            return (
+              <g>
+                <rect
+                  x={465}
+                  y={196 + index * 28}
+                  width={20}
+                  height={15}
+                  fill={COLORS[index % COLORS.length]}
+                />
+                <text
+                  x={500}
+                  y={210 + index * 28}
+                  fill={COLORS[index % COLORS.length]}
+                  fontWeight="bold"
+                  fontSize={18}
+                >
+                  {value} ({percent})
+                </text>
+                <text
+                  x={255}
+                  y={155}
+                  textAnchor="middle"
+                  fill="#333"
+                  fontSize={24}
+                  fontWeight="bold"
+                >
+                  {currentMonth}
+                </text>
+              </g>
+            );
+          }}
+        />
       </Pie>
-      <Label
-        content={({ percent }) => `${(percent * total).toFixed(0)}%`}
-        position="center"
-        fontSize={20}
-      />
       <Tooltip />
     </PieChart>
   );
@@ -49,4 +103,4 @@ const GraphCircularChart = ({ data }) => {
 
 export default GraphCircularChart;
 
-const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+const COLORS = ['#0088FE', '#00C49F', '#FFBB28'];
